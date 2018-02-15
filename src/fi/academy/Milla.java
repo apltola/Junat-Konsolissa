5fc247df932b5f7961911def5f102788fdea42c7
Elina <elina0rauhala@gmail.com>
2018-02-15 16:27:36 +0200

    Virheenkäsittely

--- src/fi/academy/Milla.java
+++ src/fi/academy/Milla.java
@@ -3,63 +3,73 @@ package fi.academy;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-import static java.util.Collections.replaceAll;
-
-//Milla koodaa junien hakua junan numeron perusteella
 public class Milla  extends UusiHakija{
 
     public static void main(String[] args) {
-        new Milla().haeNumeronPerusteella(); }
+        new Milla().kysyJunannumero();
+    }
 
-    public void haeNumeronPerusteella() {
-
+    public void kysyJunannumero() throws NumberFormatException {
         Scanner lukija = new Scanner(System.in);
-        System.out.print("\nAnna junan numero: ");
-        String junanNumero = lukija.nextLine();
-        int pelkkaNumero = Integer.parseInt(junanNumero.replaceAll("\\D", "").replaceAll("\\s", ""));
+        String junanNumero;
+        int pelkkaNumero;
+        while (true) {
+            try {
+                System.out.print("\nAnna junan numero: ");
+                junanNumero = lukija.nextLine();
+                pelkkaNumero = Integer.parseInt(junanNumero.replaceAll("\\D", "").replaceAll("\\s", ""));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Tuntematon junannumero");
+                continue;
+            }
+        }
+        System.out.println("\nLadataan tietoja...");
+        haeNumeronPerusteella(pelkkaNumero);
+    }
+
+    public void haeNumeronPerusteella(int numero) {
 
         String alkuUrl = "https://rata.digitraffic.fi/api/v1/";
         try {
-            URL urlLiike = new URL(alkuUrl + "trains/latest/" + pelkkaNumero);
+            URL urlLiike = new URL(alkuUrl + "trains/latest/" + numero);
             ObjectMapper mapper = new ObjectMapper();
-            CollectionType millan = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
-            List<Juna> junaLista = mapper.readValue(urlLiike, millan);
+            CollectionType tyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+            List<Juna> junaLista = mapper.readValue(urlLiike, tyyppi);
 
             if (!junaLista.get(0).isRunningCurrently()) {
                 System.out.println("\nJuna ei ole tällä hetkellä liikkeessä");
             } else {
                 try {
-                    URL urlAsemat = new URL(alkuUrl + "train-tracking/latest/" + pelkkaNumero + "?version=1000");
+                    URL urlAsemat = new URL(alkuUrl + "train-tracking/latest/" + numero + "?version=1000");
                     ObjectMapper mapperUusi = new ObjectMapper();
-                    CollectionType millanUusi = mapperUusi.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
-                    List<Juna> junaUusiLista = mapperUusi.readValue(urlAsemat, millanUusi);
+                    CollectionType tyyppiUusi = mapperUusi.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+                    List<Juna> junaUusiLista = mapperUusi.readValue(urlAsemat, tyyppiUusi);
+
                     String nykyinenAsema = haeAsema(junaUusiLista.get(0).getStation());
-                    if (nykyinenAsema.equals("VIRHE")) { nykyinenAsema = junaUusiLista.get(0).getStation();}
-                    if (junaUusiLista.get(0).getStation() == null) {nykyinenAsema = " ";}
+
+                    if (nykyinenAsema.equals("VIRHE"))
+                        nykyinenAsema = junaUusiLista.get(0).getStation();
+                    if (junaUusiLista.get(0).getStation() == null)
+                        nykyinenAsema = " ";
+
                     String seuraavaAsema = haeAsema(junaUusiLista.get(0).getNextStation());
-                   if (seuraavaAsema.equals("VIRHE")) { seuraavaAsema = junaUusiLista.get(0).getNextStation();}
-                    if (junaUusiLista.get(0).getNextStation() == null) {seuraavaAsema = " ";}
-                    String asemat = "\nVälillä: " + nykyinenAsema + " - "
-                            + seuraavaAsema;
-                    System.out.println(asemat);
-                   // System.out.println("Klo: " + new Date().toString().substring(11, 16));
-                } catch(IOException f){
-                    System.out.println("\nVIRHE: " + f); } }
+                    if (seuraavaAsema.equals("VIRHE"))
+                        seuraavaAsema = junaUusiLista.get(0).getNextStation();
+                    if (junaUusiLista.get(0).getNextStation() == null)
+                        seuraavaAsema = " ";
+                    System.out.println("\nVälillä: " + nykyinenAsema + " - " + seuraavaAsema);
+
+                } catch (Exception f){
+                    System.out.println(f);
+                }
+            }
         } catch (Exception e) {
             System.out.println("\nTuntematon junan numero"); }
-
-
-}
-
-
-
+    }
 }
