cdb24f8eed1358fb914a350c32dc1300f3ebd568
allu <aleksijaakkojuhani@gmail.com>
2018-02-16 10:14:21 +0200

    mainin koodia korjailtu

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -1,12 +1,11 @@
 package fi.academy;
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     private static final Scanner lukija = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // write your code here
         Kayttoliittyma kl = new Kayttoliittyma(lukija);
         kl.kayta();
 
